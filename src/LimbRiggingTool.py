a074a00a5206353b294f46f60cb4e52e566176ec
Lynn <wlynn@student.uiwtx.edu>
2025-04-15 09:00:34 -0500

    initial commit

---
+++ src/LimbRiggingTool.py
@@ -0,0 +1,204 @@
+import maya.cmds as mc
+import maya.OpenMayaUI as omui
+import shiboken2
+import maya.mel as mel
+from maya.OpenMaya import MVector
+from PySide2.QtGui import QColor
+
+from PySide2.QtWidgets import (QColorDialog, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QSlider, QPushButton, QLineEdit, QMessageBox)
+from PySide2.QtCore import Qt
+
+def GetMayaMainWindow()->QMainWindow:
+    mayaMainWindow = omui.MQtUtil.mainWindow()
+    return shiboken2.wrapInstance(int(mayaMainWindow), QMainWindow)
+
+def DeleteWindowWithName(name):
+    for window in GetMayaMainWindow().findChildren(QWidget, name):
+        window.deleteLater()
+
+class QMayaWindow(QWidget):
+    def __init__(self):
+        DeleteWindowWithName(self.GetWindowHash())
+        super().__init__(parent = GetMayaMainWindow())
+        self.setWindowFlags(Qt.WindowType.Window)
+        self.setObjectName(self.GetWindowHash())
+
+    def GetWindowHash(self):
+        return "babababababab"
+    
+class LimbRigger:
+    def __init__(self):
+        self.root = ""
+        self.mid = ""
+        self.end = ""
+        self.controllerSize = 5
+
+    def AutoFindJnts(self):
+        self.root = mc.ls(sl=True, type="joint")[0]
+        self.mid = mc.listRelatives(self.root, c=True, type="joint")[0]
+        self.end = mc.listRelatives(self.mid, c=True, type="joint")[0]
+
+    def CreateFKControlForJnt(self, jntName):
+        ctrlName = "ac_fk_" + jntName
+        ctrlGrpName = ctrlName + "_grp"
+        mc.circle(n=ctrlName, r=self.controllerSize, nr = (1,0,0))
+
+        mc.group(ctrlName, n=ctrlGrpName)
+        mc.matchTransform(ctrlGrpName, jntName)
+        mc.orientConstraint(ctrlName, jntName)
+        return ctrlName, ctrlGrpName
+    
+    def CreateBoxController(self, name):
+        mel.eval(f"curve -n {name} -d 1 -p -0.5 0.5 0.5 -p 0.5 0.5 0.5 -p 0.5 -0.5 0.5 -p -0.5 -0.5 0.5 -p -0.5 0.5 0.5 -p -0.5 0.5 -0.5 -p -0.5 -0.5 -0.5 -p -0.5 -0.5 0.5 -p 0.5 -0.5 0.5 -p 0.5 -0.5 -0.5 -p -0.5 -0.5 -0.5 -p -0.5 0.5 -0.5 -p 0.5 0.5 -0.5 -p 0.5 -0.5 -0.5 -p 0.5 -0.5 0.5 -p 0.5 0.5 0.5 -p 0.5 0.5 -0.5 -k 0 -k 1 -k 2 -k 3 -k 4 -k 5 -k 6 -k 7 -k 8 -k 9 -k 10 -k 11 -k 12 -k 13 -k 14 -k 15 -k 16")
+        mc.scale(self.controllerSize, self.controllerSize, self.controllerSize, name)
+        mc.makeIdentity(name, apply = True) #freezes transformes
+        
+        grpName = name + "_grp"
+        mc.group(name, n=grpName)
+        return name, grpName
+    
+    def CreatePlusController(self, name):
+        mel.eval (f"curve -n {name} -d 1 -p 1 3 0 -p -1 3 0 -p -1 1 0 -p -3 1 0 -p -3 -1 0 -p -1 -1 0 -p -1 -3 0 -p 1 -3 0 -p 1 -1 0 -p 3 -1 0 -p 3 1 0 -p 1 1 0 -p 1 3 0 -k 0 -k 1 -k 2 -k 3 -k 4 -k 5 -k 6 -k 7 -k 8 -k 9 -k 10 -k 11 -k 12")
+        # mc.scale(self.controllerSize/3, self.controllerSize/3, self.controllerSize/3, name)
+        # mc.makeIdentity(name, apply = True) 
+        
+        grpName = name + "_grp"
+        mc.group(name, n=grpName)
+        return name, grpName
+    
+    def GetObjectLoc(self, objectName)->MVector: 
+        x, y, z = mc.xform(objectName, q=True, t=True, ws=True)
+        return MVector (x, y, z)
+    
+    def PrintMVector(self, vectorToPrint):
+        print(f"{vectorToPrint.x}, {vectorToPrint.y}, {vectorToPrint.z}")
+
+    def RigLimb(self, r, g, b):
+        rootFKCtrl, rootFKCtrlGrp = self.CreateFKControlForJnt(self.root)
+        midFKCtrl, midFKCtrlGrp = self.CreateFKControlForJnt(self.mid)
+        endFKCtrl, endFKCtrlGrp = self.CreateFKControlForJnt(self.end)
+
+        mc.parent(midFKCtrlGrp, rootFKCtrl)
+        mc.parent(endFKCtrlGrp, midFKCtrl)
+
+        ikEndCtrl = "ac_ik_" + self.end
+        ikEndCtrl, ikEndCtrlGrp = self.CreateBoxController(ikEndCtrl)
+        mc.matchTransform(ikEndCtrlGrp, self.end)
+        endOrientConstraint = mc.orientConstraint(ikEndCtrl, self.end)[0]
+
+        rootJntLoc = self.GetObjectLoc(self.root)
+        endJntLoc = self.GetObjectLoc(self.end)
+        
+        rootToEndVec = endJntLoc - rootJntLoc
+
+        ikHandleName = "ikHandle_" + self.end
+        mc.ikHandle(n=ikHandleName, sj=self.root, ee = self.end, sol="ikRPsolver")
+        ikPoleVectorVals = mc.getAttr(ikHandleName + ".poleVector")[0]
+        ikPoleVector = MVector(ikPoleVectorVals[0], ikPoleVectorVals[1], ikPoleVectorVals[2])
+
+        ikPoleVector.normalize()
+        ikPoleVectorCtrlLoc = rootJntLoc + rootToEndVec / 2 + ikPoleVector * rootToEndVec.length()
+
+        ikPoleVectorCtrlName = "ac_ik_" + self.mid
+        mc.spaceLocator(n=ikPoleVectorCtrlName)
+        ikPoleVectorCtrlGrp = ikPoleVectorCtrlName + "_grp"
+        mc.group(ikPoleVectorCtrlName, n=ikPoleVectorCtrlGrp)
+        mc.setAttr(ikPoleVectorCtrlGrp+".t", ikPoleVectorCtrlLoc.x, ikPoleVectorCtrlLoc.y, ikPoleVectorCtrlLoc.z, typ = "double3")
+        mc.poleVectorConstraint(ikPoleVectorCtrlName, ikHandleName)
+
+        ikfkBlendCtrlName = "ac_ikfk_blend_" + self.root
+        ikfkBlendCtrlName, ikfkBlendCtrlGrp = self.CreatePlusController(ikfkBlendCtrlName)
+        ikfkBlendCtrlLoc = rootJntLoc + MVector(rootJntLoc.x, 0, rootJntLoc.z)
+        mc.setAttr(ikfkBlendCtrlGrp+".t", ikfkBlendCtrlLoc.x, ikfkBlendCtrlLoc.y, ikfkBlendCtrlLoc.z, typ="double3")
+
+        ikfkBlendAttrName = "ikfkBlend"
+        mc.addAttr(ikfkBlendCtrlName, ln=ikfkBlendAttrName, min=0, max=1, k=True)
+        ikfkBlendAttr = ikfkBlendCtrlName + "." + ikfkBlendAttrName
+
+        mc.expression(s=f"{ikHandleName}.ikBlend = {ikfkBlendAttr}")
+        mc.expression(s=f"{ikEndCtrlGrp}.v = {ikPoleVectorCtrlGrp}.v = {ikfkBlendAttr}")
+        mc.expression(s=f"{rootFKCtrlGrp}.v = 1 - {ikfkBlendAttr}")
+        mc.expression(s=f"{endOrientConstraint}.{endFKCtrl}W0 = 1-{ikfkBlendAttr}")
+        mc.expression(s=f"{endOrientConstraint}.{ikEndCtrl}W1 = {ikfkBlendAttr}")
+
+        mc.parent(ikHandleName, ikEndCtrl)
+        mc.setAttr(ikHandleName+".v", 0)
+
+        topGrpName = self.root + "_rig_grp"
+        mc.group([rootFKCtrlGrp, ikEndCtrlGrp, ikPoleVectorCtrlGrp, ikfkBlendCtrlGrp], n= topGrpName)
+        mc.setAttr(topGrpName+".overrideEnabled", 1)
+        mc.setAttr(topGrpName+".overrideRGBColors", 1)
+        mc.setAttr(topGrpName+".overrideColorRGB", r, g, b, type="double3")
+
+class ColorPicker(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.masterLayout = QVBoxLayout()
+        self.setLayout(self.masterLayout)
+        self.colorPickerBtn = QPushButton()
+        self.colorPickerBtn.setStyleSheet(f"background-color:black")
+        self.masterLayout.addWidget(self.colorPickerBtn)
+        self.colorPickerBtn.clicked.connect(self.ColorPickerBtnClicked)
+        self.color = QColor(0,0,0)
+
+    def ColorPickerBtnClicked(self):
+        self.color = QColorDialog.getColor()
+        self.colorPickerBtn.setStyleSheet(f"background-color:{self.color.name()}")
+
+
+class LimbRigToolWidget(QMayaWindow):
+    def __init__(self):
+        super().__init__()
+        self.rigger = LimbRigger()
+        self.masterLayout = QVBoxLayout()
+        self.setLayout(self.masterLayout)
+
+        self.tipLabel = QLabel("Select the First Joint of the Limb, and click on the Auto Find Button")
+        self.masterLayout.addWidget(self.tipLabel)
+
+        self.jointSelectionText = QLineEdit()
+        self.masterLayout.addWidget(self.jointSelectionText)
+        self.jointSelectionText.setEnabled(False)
+
+        self.autoFindBtn = QPushButton("Auto Find")
+        self.masterLayout.addWidget(self.autoFindBtn)
+        self.autoFindBtn.clicked.connect(self.AutoFindBtnClicked)
+
+        ctrlSliderLayout = QHBoxLayout()
+        ctrlSizeSlider = QSlider()
+        ctrlSizeSlider.setValue(self.rigger.controllerSize)
+        ctrlSizeSlider.valueChanged.connect(self.CtrlSizeValueChanged)
+        ctrlSizeSlider.setRange(1,15)
+        ctrlSizeSlider.setOrientation(Qt.Horizontal)
+        ctrlSliderLayout.addWidget(ctrlSizeSlider)
+        self.ctrlSizeLabel = QLabel(f"{self.rigger.controllerSize}")
+        ctrlSliderLayout.addWidget(self.ctrlSizeLabel)
+
+        self.masterLayout.addLayout(ctrlSliderLayout)
+
+        self.colorPicker = ColorPicker()
+        self.masterLayout.addWidget(self.colorPicker)
+
+        self.rigLimbBtn = QPushButton("Rig Limb")
+        self.masterLayout.addWidget(self.rigLimbBtn)
+        self.rigLimbBtn.clicked.connect(self.RigLimbBtnClicked)
+
+        self.setWindowTitle("Limb Rigging Tool")
+
+    def CtrlSizeValueChanged(self, newValue):
+        self.rigger.controllerSize = newValue
+        self.ctrlSizeLabel.setText(f"{self.rigger.controllerSize}")
+
+    def RigLimbBtnClicked(self):
+        self.rigger.RigLimb(self.colorPicker.color.redF(), self.colorPicker.color.greenF(), self.colorPicker.color.blueF())
+        
+    def AutoFindBtnClicked(self):
+        try:
+            self.rigger.AutoFindJnts()
+            self.jointSelectionText.setText(f"{self.rigger.root}, {self.rigger.mid}, {self.rigger.end}")
+        except Exception as e:
+            QMessageBox.critical(self, "Error", "Wrong Selection, please select the first joint of Limb!")
+
+limbRigToolWidget = LimbRigToolWidget()
+limbRigToolWidget.show()
+
---
+++ src/ProxyRigger.py
@@ -0,0 +1,154 @@
+import importlib
+import MayaUtils
+importlib.reload(MayaUtils)
+
+from MayaUtils import *
+from PySide2.QtWidgets import QPushButton, QVBoxLayout
+import maya.cmds as mc
+
+class ProxyRigger:
+    def __init__(self):
+        self.skin = ""
+        self.model = ""
+        self.jnts = []
+
+    def CreatProxyRigFromSelectedMesh(self):
+        mesh = mc.ls(sl=True)[0]
+        if not IsMesh(mesh):
+            raise TypeError(f"{mesh} is not a mesh! please select a mesh")
+        
+        self.model = mesh
+        modelShape = mc.listRelatives(self.model, s=True)[0]
+        print (f"found mesh {mesh} and shape {modelShape}")
+
+        skin = GetAllConnectIn(modelShape, GetUpperStream, 10, IsSkin)
+        if not skin:
+            raise Exception(f"{mesh} has no skin! this tool only works with a rigged model")
+        self.skin = skin[0]
+
+        jnts = GetAllConnectIn(modelShape, GetUpperStream, 10, IsJoint)
+        if not jnts:
+            raise Exception(f"{mesh} has no joints bound! this tool only works with a rigged model")
+        self.jnts = jnts
+
+        print(f"start build with mesh: {self.model}, skin: {self.skin} and joints: {self.jnts}")
+
+        jntVertMap = self.GenerateJntVertDict()
+        segments = []
+        ctrls = []
+        for jnt, verts in jntVertMap.items():
+            print(f"joint {jnt} controls {verts} primarily")
+            newSeg = self.CreateProxyModelForJntsAndVerts(jnt, verts)
+            if newSeg is None:
+                continue
+
+            newSkinCluster = mc.skinCluster(self.jnts, newSeg)[0]
+            mc.copySkinWeights(ss=self.skin, ds=newSkinCluster, nm=True, sa="closestPoint", ia="closestJoint")
+            segments.append(newSeg)
+
+            ctrlLocator = "ac_" + jnt + "_proxy"
+            mc.spaceLocator(n=ctrlLocator)
+            ctrlLocatorGrp = ctrlLocator + "_grp"
+            mc.group(ctrlLocator, n=ctrlLocatorGrp)
+            mc.matchTransform(ctrlLocatorGrp, jnt)
+            
+            visibilityAttr = "vis"
+            mc.addAttr(ctrlLocator, ln=visibilityAttr, min=0, max=1, dv=1, k=True)
+            mc.connectAttr(ctrlLocator + "." + visibilityAttr, newSeg + ".v")
+            ctrls.append(ctrlLocatorGrp)
+
+        proxyTopGrp = self.model + "_proxy_grp"
+        mc.group(segments, n=proxyTopGrp)
+
+        ctrlTopGrp = "ac_" + self.model + "_proxy_grp"
+        mc.group(ctrls, n=ctrlTopGrp)
+
+        globalProxyCtrl = "ac_" + self.model + "_proxy_global"
+        mc.circle(n=globalProxyCtrl, r=30)
+        mc.parent(proxyTopGrp, globalProxyCtrl)
+        mc.parent(ctrlTopGrp, globalProxyCtrl)
+        mc.setAttr(proxyTopGrp + ".inheritsTransforms", 0)
+
+        visibilityAttr = "vis"
+        mc.addAttr(globalProxyCtrl, ln=visibilityAttr, min=0, max=1, dv=1, k=True)
+        mc.connectAttr(globalProxyCtrl + "." + visibilityAttr, proxyTopGrp + ".v")
+
+    def CreateProxyModelForJntsAndVerts (self, jnt, verts):
+        if not verts:
+            return None
+        
+        faces = mc.polyListComponentConversion(verts, fromVertex=True, toFace=True)
+        faces = mc.ls(faces, fl=True)
+
+        labels = set()
+        for face in faces:
+            labels.add(face.replace(self.model, ""))
+
+        dup = mc.duplicate(self.model)[0]
+
+        allDupFaces = mc.ls(f"{dup}.f[*]", fl=True)
+        facesToDelete = []
+        for dupFace in allDupFaces:
+            label = dupFace.replace(dup, "")
+            if label not in labels:
+                facesToDelete.append(dupFace)
+
+        mc.delete(facesToDelete)
+
+        dupName = self.model + "_" + jnt + "_proxy"
+        mc.rename(dup, dupName)
+        return dupName
+
+
+    def GenerateJntVertDict(self):
+        dict = {}
+        for jnt in self.jnts:
+            dict[jnt] = []
+
+        verts = mc.ls(f"{self.model}.vtx[*]", fl=True)
+        for vert in verts:
+            owningJnt = self.GetJntWithMaxInfluence(vert, self.skin)
+            dict[owningJnt].append(vert)
+
+        return dict
+
+    def GetJntWithMaxInfluence(self, vert, skin):
+        weights = mc.skinPercent(skin, vert, q=True, v=True)
+        jnts = mc.skinPercent(skin, vert, q=True, t=None)
+
+        maxWeightIndex = 0
+        maxWeight = weights[0]
+
+        for i in range(1, len(weights)):
+            if weights[i] > maxWeight:
+                maxWeight = weights[i]
+                maxWeightIndex = i
+
+        return jnts[maxWeightIndex]
+
+
+
+
+class ProxyRiggerWidget(QMayaWindow):
+    def __init__(self):
+        super().__init__()
+        self.proxyRigger = ProxyRigger()
+        self.setWindowTitle("Proxy Rigger")
+        self.masterLayout = QVBoxLayout()
+        self.setLayout(self.masterLayout)
+        generateProxyRigBtn = QPushButton("Generate Proxy Rig")
+        self.masterLayout.addWidget(generateProxyRigBtn)
+        generateProxyRigBtn.clicked.connect(self.GenerateProxyRigButtonClicked)
+
+    def GenerateProxyRigButtonClicked(self):
+        self.proxyRigger.CreatProxyRigFromSelectedMesh()
+        
+
+    def GetWindowHash(self):
+        return "s685alinsejkn1526gvjsdfjhn2345sjdnfgnjdnf2563ljxbnxnlmz109jns"
+
+    
+
+proxyRiggerWidget = ProxyRiggerWidget()
+proxyRiggerWidget.show()
+
